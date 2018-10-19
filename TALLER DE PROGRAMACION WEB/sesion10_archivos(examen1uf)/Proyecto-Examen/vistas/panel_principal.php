405b872f18c97e9144d2212dce7e1d476745aa3f
mxcat1 <mxcat.xxtremxx@gmail.com>
2018-10-19 14:34:16 -0500

    Signed-off-by: mxcat1 <mxcat.xxtremxx@gmail.com>

--- TALLER DE PROGRAMACION WEB/sesion10_archivos(examen1uf)/Proyecto-Examen/vistas/panel_principal.php
+++ TALLER DE PROGRAMACION WEB/sesion10_archivos(examen1uf)/Proyecto-Examen/vistas/panel_principal.php
@@ -33,4 +33,53 @@
 
         </div>
     </div>
+    <div id="boton_pdf">
+        <button type="button" onclick="crear_pdf()">Crear PDF</button>
+    </div>
+    <script>
+        function crear_pdf() {
+            html2canvas(document.getElementById('resultado')).then(function (canvas) {
+
+
+                let contentWidth = canvas.width;
+                let contentHeight = canvas.height;
+
+                //The height of the canvas which one pdf page can show;
+                let pageHeight = contentWidth / 592.28 * 841.89;
+                //the height of canvas that haven't render to pdf
+                let leftHeight = contentHeight;
+                //addImage y-axial offset
+                let position = 0;
+                //a4 format [595.28,841.89]
+                let imgWidth = 595.28;
+                let imgHeight = 592.28/contentWidth * contentHeight;
+
+                let pageData = canvas.toDataURL('image/jpeg', 1.0);
+
+                let pdf = new jsPDF('', 'pt', 'a4');
+
+                if (leftHeight < pageHeight) {
+                    pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+                } else {
+                    while(leftHeight > 0) {
+                        pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+                        leftHeight -= pageHeight;
+                        position -= 841.89;
+                        //avoid blank page
+                        if(leftHeight > 0) {
+                            pdf.addPage();
+                        }
+                    }
+                }
+                pdf.save('reportes.pdf')
+                //
+                //
+                // let img=canvas.toDataURL('image/png')
+                // let doc = new jsPDF();
+                // doc.addImage(img,'JPGE',20,20)
+                // doc.save('reporte1.pdf')
+                // document.body.appendChild(canvas);
+            })
+        }
+    </script>
 </div>
